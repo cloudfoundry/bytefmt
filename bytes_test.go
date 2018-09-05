d429cb545c127a288507b7266c8a9f5a931c9916
Evgeniy Kulikov <im@kulikov.im>
2018-09-05 11:02:38 +0300

    Add benchmark
    ```
    name        old time/op    new time/op    delta
    ToBytes-8      569ns ± 5%     148ns ± 3%  -73.97%  (p=0.000 n=9+10)
    ByteSize-8     441ns ± 5%     276ns ± 4%  -37.41%  (p=0.000 n=10+9)
    
    name        old alloc/op   new alloc/op   delta
    ToBytes-8       118B ± 0%       16B ± 0%  -86.44%  (p=0.000 n=10+10)
    ByteSize-8     44.0B ± 0%     40.0B ± 0%   -9.09%  (p=0.000 n=10+10)
    
    name        old allocs/op  new allocs/op  delta
    ToBytes-8       4.00 ± 0%      2.00 ± 0%  -50.00%  (p=0.000 n=10+10)
    ByteSize-8      5.00 ± 0%      3.00 ± 0%  -40.00%  (p=0.000 n=10+10)
    ```

--- bytes_test.go
+++ bytes_test.go
@@ -1,10 +1,31 @@
 package bytefmt_test
 
 import (
+	"testing"
+
 	. "code.cloudfoundry.org/bytefmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
+
+const (
+	byteSize = 1e10
+	toBytes = "\n\n\r\t10.18TiB\n\n\r\t"
+)
+
+func BenchmarkToBytes(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		ToBytes(toBytes)
+	}
+}
+
+func BenchmarkByteSize(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		ByteSize(byteSize)
+	}
+}
 
 var _ = Describe("bytefmt", func() {
 
